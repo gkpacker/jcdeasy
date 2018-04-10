aca73ac71ade9a9dac0251022cb990c09a87d6b0
Gabriel Packer <ggkpacker@gmail.com>
2018-04-10 17:38:15 -0300

    panel show created

--- app/controllers/panels_controller.rb
+++ app/controllers/panels_controller.rb
@@ -1,5 +1,16 @@
 class PanelsController < ApplicationController
+  before_action :set_panel, only: [:show]
+
   def index
     @panels = Panel.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_panel
+    @panel = Panel.find(params[:id])
   end
 end

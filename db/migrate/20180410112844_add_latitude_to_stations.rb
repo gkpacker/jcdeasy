0842f0aec874db7ef770ca9e615e105875f8ecb6
Gabriel Packer <ggkpacker@gmail.com>
2018-04-10 09:10:19 -0300

    add lng/lat to stations and generate seed

---
+++ db/migrate/20180410112844_add_latitude_to_stations.rb
@@ -0,0 +1,5 @@
+class AddLatitudeToStations < ActiveRecord::Migration[5.1]
+  def change
+    add_column :stations, :latitude, :float
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+require 'pry-byebug'
+
+Line.destroy_all
+Station.destroy_all
+
+puts "Creating lines..."
+
+linha_azul = Line.create!(name: "Linha 1 - Azul")
+
+linha_verde = Line.create!(name: "Linha 2 - Verde")
+
+linha_vermelha = Line.create!(name: "Linha 3 - Vermelha")
+
+linha_lilas = Line.create!(name: "Linha 5 - Lilás")
+
+puts "Created #{Line.count} lines."
+
+
+
+
+url = open('https://gist.githubusercontent.com/rafaelrinaldi/6a82dd1eceed6dfc7deb/raw/a43679cd321e9b779f2eb53e2f9fc8279b7a3c29/stations.json')
+parsed = JSON.parse(url.read)
+
+puts "Creating stations..."
+
+parsed.each do |station_collection|
+  station_info = station_collection.last
+  if station_info["lines"].include?("linha-1-azul")
+    byebug
+    station = Station.new
+    station.name = station_info["title"]
+    station.latitude = station_info["location"]["latitude"]
+    station.longitude = station_info["location"]["longitude"]
+    station.address = station_info["location"]["address"]
+    station.lines << linha_azul
+    station.save!
+  end
+end
+
+puts "Created #{Station.count} stations."
+

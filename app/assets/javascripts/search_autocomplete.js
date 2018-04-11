86435f8dbb3f4f0997ad2caeb363442f6e8c1b31
Charlotte Boucher <charlotte.boucher@hec.edu>
2018-04-11 14:27:10 -0300

    home page

---
+++ app/assets/javascripts/search_autocomplete.js
@@ -0,0 +1,8 @@
+stations = Stations.all
+
+const list_stations = document.getElementById("search_station")
+list_stations.addEventListener("keyup", autocomplete)
+
+// const autocomplete = (event) => {
+//   event.currentTarget.
+//    }

46d0c148ba757995aaf0741ddf4bca004b7ad350
Charlotte Boucher <charlotte.boucher@hec.edu>
2018-04-12 17:25:44 -0300

    cancel change

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,7 +3,7 @@ class Company < ApplicationRecord
   belongs_to :user
   has_many :campaigns
 
-  validates :cnpj, :name, :street_number, :address1, :zip_code, :city, :state, presence: true
+  validates :cnpj, :name, :street_number, :address1, :address2, :zip_code, :city, :state, presence: true
   validate :check_cnpj
   validates_uniqueness_of :cnpj, conditions: -> { where(status: [:active]) }
 

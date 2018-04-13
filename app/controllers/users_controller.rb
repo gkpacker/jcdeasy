be2a06cf6cbffb585657d37b9e4fb61d4ebf238e
Gabriel Packer <ggkpacker@gmail.com>
2018-04-13 16:04:23 -0300

    visual logic for campaigns and companies

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
-<<<<<<< HEAD
     @all_companies = Company.where(user: @user).includes(:campaigns)
-    @companies = Company.where(user: @user, status: [:active])
-    @company = Company.new
-    @campaign = Campaign.new
-=======
     # @all_companies = Company.where(user: @user).includes(:campaigns)
     @companies = Company.where(user: @user, status: [:active]).includes(:campaigns)
     @campaigns = []
@@ -15,6 +10,7 @@ class UsersController < ApplicationController
         @campaigns << campaign
       end
     end
->>>>>>> fix index campaings
+    @company = Company.new
+    @campaign = Campaign.new
   end
 end

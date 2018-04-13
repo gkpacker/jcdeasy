0f17bf01b305e42be5d23e2d360471af44e9786f
Gabriel Packer <ggkpacker@gmail.com>
2018-04-13 15:34:58 -0300

    fix search bar layout

--- app/controllers/panels_controller.rb
+++ app/controllers/panels_controller.rb
@@ -22,7 +22,7 @@ class PanelsController < ApplicationController
     if user_signed_in?
       @dates = calculate_dates
       @order = @panel.orders.build
-      @companies = Company.where(status: :active, user: current_user)
+      @companies = Company.where(status: :active, user: current_user).includes(:campaigns)
       @campaign = Campaign.new
       @company = Company.new
       @campaigns = []
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
-    @all_companies = Company.where(user: @user)
+    @all_companies = Company.where(user: @user).includes(:campaigns)
     @companies = Company.where(user: @user, status: [:active])
     @company = Company.new
     @campaign = Campaign.new

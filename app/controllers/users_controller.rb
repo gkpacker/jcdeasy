3ac181250d0fc8d0df33845f82a953e79d58d808
Gabriel Packer <ggkpacker@gmail.com>
2018-04-13 15:48:03 -0300

    fix index campaings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,20 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
+<<<<<<< HEAD
     @all_companies = Company.where(user: @user).includes(:campaigns)
     @companies = Company.where(user: @user, status: [:active])
     @company = Company.new
     @campaign = Campaign.new
+=======
+    # @all_companies = Company.where(user: @user).includes(:campaigns)
+    @companies = Company.where(user: @user, status: [:active]).includes(:campaigns)
+    @campaigns = []
+    @companies.each do |company|
+      company.campaigns.each do |campaign|
+        @campaigns << campaign
+      end
+    end
+>>>>>>> fix index campaings
   end
 end

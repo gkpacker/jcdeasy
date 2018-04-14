c2f3a7845d506c41a63d56e24e497434877b6b75
Gabriel Packer <ggkpacker@gmail.com>
2018-04-14 12:51:50 -0300

    inputmask cep// working on payment

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,14 +1,16 @@
-function initUpdateNavbarOnScroll() {
-  const navbar = document.querySelector('.navbar-wagon');
-  window.addEventListener('scroll', () => {
-    if (window.scrollY >= window.innerHeight) {
-      navbar.classList.remove('navbar-wagon');
-      navbar.classList.add('navbar-wagon-grey');
-    } else {
-      navbar.classList.remove('navbar-wagon-grey');
-      navbar.classList.add('navbar-wagon');
-    }
-  });
+if (document.querySelector('.navbar-wagon')) {
+  function initUpdateNavbarOnScroll() {
+    const navbar = document.querySelector('.navbar-wagon');
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= window.innerHeight) {
+        navbar.classList.remove('navbar-wagon');
+        navbar.classList.add('navbar-wagon-grey');
+      } else {
+        navbar.classList.remove('navbar-wagon-grey');
+        navbar.classList.add('navbar-wagon');
+      }
+    });
+  }
 }
 
 export { initUpdateNavbarOnScroll };

0d15efa0051dda77ff6f21051e6d4b68b2a5e32a
Charlotte Boucher <charlotte.boucher@hec.edu>
2018-04-17 11:36:09 -0300

    finish JS para desabilitar button

--- app/javascript/components/disabling.js
+++ app/javascript/components/disabling.js
@@ -1,5 +1,20 @@
 const button = document.getElementById('add-panel');
 
-while ((document.getElementById('order_date') === "") || (document.getElementById("order_campaign_id").value === "")) {
-  button.setAttribute('disabled', "");
-}
+const date = document.getElementById('order_date');
+const campaign = document.getElementById("order_campaign_id");
+
+date.addEventListener('change', (event) => {
+  if (!(campaign.value === "")) {
+    button.removeAttribute('disabled')
+  }
+});
+
+campaign.addEventListener('change', (event) => {
+  if (!(date.value === "")) {
+    button.removeAttribute('disabled')
+  }
+});
+
+// while ((document.date === "") || (document.campaign.value === "")) {
+//   button.setAttribute('disabled', "");
+// }

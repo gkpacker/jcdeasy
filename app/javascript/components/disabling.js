2d563f633f6ded2f6acc71c894a536cd483802bf
Gabriel Packer <ggkpacker@gmail.com>
2018-04-19 12:56:36 -0300

    disable campaign  button

--- app/javascript/components/disabling.js
+++ app/javascript/components/disabling.js
@@ -1,8 +1,12 @@
 const bindDisableForm = () => {
   const button = document.getElementById('add-panel');
+  const buttonCampaign = document.getElementById('add-campaign');
 
   const date = document.getElementById('order_date');
   const campaign = document.getElementById("order_campaign_id");
+
+  const title = document.getElementById('campaign_title');
+  const company = document.getElementById("campaign_company");
 
   date.addEventListener('change', (event) => {
     if (!(campaign.value === "")) {
@@ -15,5 +19,29 @@ const bindDisableForm = () => {
       button.removeAttribute('disabled')
     }
   })
+
+  title.addEventListener('keyup', (event) => {
+    if (!(company.value === "")) {
+      buttonCampaign.removeAttribute('disabled')
+    } else {
+      buttonCampaign.setAttribute('disabled', 'disabled')
+    }
+    if (title.value === "") {
+      buttonCampaign.setAttribute('disabled', 'disabled')
+    }
+  });
+
+  company.addEventListener('change', (event) => {
+    if (!(title.value === "")) {
+      buttonCampaign.removeAttribute('disabled')
+    } else {
+      buttonCampaign.setAttribute('disabled', 'disabled')
+    }
+    if (company.value === "") {
+      buttonCampaign.setAttribute('disabled', 'disabled')
+    }
+  })
 };
+
+
 export { bindDisableForm }
